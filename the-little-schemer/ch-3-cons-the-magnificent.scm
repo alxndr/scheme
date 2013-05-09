7119f6e6c798af62b5880290e7d83dfc658a75e9
Alexander <alxndr+github@gmail.com>
2013-05-08 22:51:51 -0700

    multisubst to close out ch 3

--- the-little-schemer/ch-3-cons-the-magnificent.scm
+++ the-little-schemer/ch-3-cons-the-magnificent.scm
@@ -114,4 +114,15 @@
   )
 )
 
+(define multisubst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons new (multisubst new old (cdr lat))))
+      (else
+        (cons (car lat) (multisubst new old (cdr lat)))
+      )
+    )
+  )
+)
 
