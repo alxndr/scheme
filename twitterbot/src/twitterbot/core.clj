27e24de191730aa97cf89721a5b640f0b335814b
Alexander <alxndr+github@gmail.com>
2013-12-04 23:43:31 -0800

    clojure: named parameters, sorta

--- twitterbot/src/twitterbot/core.clj
+++ twitterbot/src/twitterbot/core.clj
@@ -1,4 +1,5 @@
-(ns twitterbot.core)
+(ns twitterbot.core
+    )
 
 (defn to-str
   "convenience fn to concat & call str"
@@ -6,16 +7,15 @@
   (apply str (clojure.core/concat seqs)))
 
 (defn trim-head
-  "if text starts with head (optionally surrounded by whitespace),
-   strip it off"
-  [text head]
+  "if text starts with head (optionally surrounded by whitespace), strip it off"
+  [& {:keys [text head]}]
   (let [head-regex (re-pattern (to-str "^ *@" head " +"))]
     (clojure.string/replace text head-regex "")))
 
 (defn rewrite-tweet
   "reformat tweet-text by writer which mentions reader"
   [tweet-text writer reader]
-  (let [trimmed-tweet (trim-head tweet-text (to-str reader))]
+  (let [trimmed-tweet (trim-head :text tweet-text :head (to-str reader))]
     (to-str "{@" writer "} " trimmed-tweet)))
 
 (defn -main []
--- twitterbot/test/twitterbot/core_test.clj
+++ twitterbot/test/twitterbot/core_test.clj
@@ -1,29 +1,25 @@
 (ns twitterbot.core-test
   (:require [clojure.test :refer :all]
             [twitterbot.core :refer :all]))
+
 (deftest rewrite-tweet-test
 
-  (testing "prepends writer's name"
-           (is (= "{@writer} foo bar"
-                  (rewrite-tweet "@reader foo bar"
-                                 "writer"
-                                 "reader"))))
+  (testing
+    "prepends writer's name"
+    (is (= "{@writer} foo bar"
+           (rewrite-tweet "@reader foo bar" "writer" "reader"))))
 
-  (testing "ignores leading whitespace"
-           (is (= "{@writer} foo bar"
-                  (rewrite-tweet " @reader foo bar"
-                                 "writer"
-                                 "reader"))))
+  (testing
+    "ignores leading whitespace"
+    (is (= "{@writer} foo bar"
+           (rewrite-tweet " @reader foo bar" "writer" "reader"))))
 
-  (testing "ignores non-leading @reader"
-           (is (= "{@writer} foo @reader bar"
-                  (rewrite-tweet "foo @reader bar"
-                                 "writer"
-                                 "reader")))
-           (is (= "{@writer} foo @reader bar"
-                  (rewrite-tweet "@reader foo @reader bar"
-                                 "writer"
-                                 "reader"))))
+  (testing
+    "ignores non-leading @reader"
+    (is (= "{@writer} foo @reader bar"
+           (rewrite-tweet "foo @reader bar" "writer" "reader")))
+    (is (= "{@writer} foo @reader bar"
+           (rewrite-tweet "@reader foo @reader bar" "writer" "reader"))))
 
 )
 
