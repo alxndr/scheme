f6fab2ac6d006e2a75f1ed6e1c3c3b8664dac1e5
Alexander <alxndr+github@gmail.com>
2013-12-09 07:55:24 -0800

    renaming, and trying to use twitter

---
+++ twamplifier/project.clj
@@ -0,0 +1,10 @@
+(defproject twamplifier "0.0.1"
+  :description "a twitter bot that reposts things"
+  :url "nope"
+  :license {:name "CC BY-NC-SA"
+            :url "google it"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [twitter-api "0.7.4"]
+                 [clj-http "0.7.7"]]
+  :main twamplifier.core
+  )
--- twamplifier/test/twamplifier/core_test.clj
+++ twamplifier/test/twamplifier/core_test.clj
@@ -1,24 +1,24 @@
-(ns twitterbot.core-test
+(ns twamplifier.core-test
   (:require [clojure.test :refer :all]
-            [twitterbot.core :refer :all]))
+            [twamplifier.core :refer :all]))
 
 (deftest rewrite-tweet-test
 
   (testing
     "prepends writer's name"
-    (is (= "{@writer} foo bar"
+    (is (= "[@writer] foo bar"
            (rewrite-tweet "@reader foo bar" "writer" "reader"))))
 
   (testing
     "ignores leading whitespace"
-    (is (= "{@writer} foo bar"
+    (is (= "[@writer] foo bar"
            (rewrite-tweet " @reader foo bar" "writer" "reader"))))
 
   (testing
     "ignores non-leading @reader"
-    (is (= "{@writer} foo @reader bar"
+    (is (= "[@writer] foo @reader bar"
            (rewrite-tweet "foo @reader bar" "writer" "reader")))
-    (is (= "{@writer} foo @reader bar"
+    (is (= "[@writer] foo @reader bar"
            (rewrite-tweet "@reader foo @reader bar" "writer" "reader"))))
 
 )
