1ac7420ccc0f7e5a91cfa46a2c1e58e3c80a8c8f
Alexander <alxndr+github@gmail.com>
2020-11-18 20:52:06 -0800

    TLS ch4: `plus` and `minus`

---
+++ little-schemer/ch-4-numbers-games.scm
@@ -0,0 +1,17 @@
+(define add1 (lambda (n)
+               (+ n 1)))
+(define sub1 (lambda (n)
+               (- n 1)))
+
+(define plus (lambda (a b)
+               (cond
+                 ((zero? a) b)
+                 (else
+                   (plus (sub1 a) (add1 b))))))
+                   ; (add1 (plus a (sub1 b))))))) ; less efficient??
+
+(define minus (lambda (a b)
+                (cond
+                  ((zero? b) a)
+                  (else
+                    (minus (sub1 a) (sub1 b))))))
