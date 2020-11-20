6f879cbf9650d0f4f5efeb09110969a3e2f049e4
Alexander <alxndr+github@gmail.com>
2020-11-19 18:48:36 -0800

    TLS ch4: `no-nums`

--- little-schemer/ch-4-numbers-games.scm
+++ little-schemer/ch-4-numbers-games.scm
@@ -79,3 +79,9 @@
                     ((null? lat) '())
                     ((zero? (sub1 n)) (cdr lat))
                     (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums (lambda (lat)
+                  (cond
+                    ((null? lat) '())
+                    ((number? (car lat)) (no-nums (cdr lat)))
+                    (else (cons (car lat) (no-nums (cdr lat)))))))
