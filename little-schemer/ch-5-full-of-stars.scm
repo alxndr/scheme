9a70b4978a41d44059359915e34506a5f5d3ee50
Alexander <alxndr+github@gmail.com>
2020-11-19 20:34:06 -0800

    TLS ch5: `insertR*`

--- little-schemer/ch-5-full-of-stars.scm
+++ little-schemer/ch-5-full-of-stars.scm
@@ -8,3 +8,13 @@
                      (cons (car l) (rember* a (cdr l))))
                     (else
                       (cons (rember* a (car l)) (rember* a (cdr l)))))))
+
+(define insertR* (lambda (new old l)
+                   (cond
+                     ((null? l) '())
+                     ((and (atom? (car l)) (eqan? (car l) old))
+                      (cons old (cons new (insertR* new old (cdr l)))))
+                     ((atom? (car l))
+                      (cons (car l) (insertR* new old (cdr l))))
+                     (else
+                       (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
