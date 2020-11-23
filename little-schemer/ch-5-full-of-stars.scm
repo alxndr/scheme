557baf79ff2cbe340614c6bc80dec3a1bae80287
Alexander <alxndr+github@gmail.com>
2020-11-23 08:40:56 -0800

    TLS ch5: WIP `eqlist?`

--- little-schemer/ch-5-full-of-stars.scm
+++ little-schemer/ch-5-full-of-stars.scm
@@ -65,3 +65,25 @@
                      ((null? l) '())
                      ((atom? (car l)) (car l))
                      (else (leftmost (car l))))))
+
+(define
+  eqlist? (lambda (l1 l2)
+            (cond
+              ((and
+                 (null? l1)
+                 (null? l2))
+               #t)
+              ((or
+                 (null? l1)
+                 (null? l2))
+               #f)
+              ((and
+                 (atom? (car l1))
+                 (atom? (car l2)))
+               (and
+                 (eq?     (car l1) (car l2))
+                 (eqlist? (cdr l1) (cdr l2))))
+              (else
+                (and
+                  (eqlist? (car l1) (car l2))
+                  (eqlist? (cdr l1) (cdr l2)))))))
