17bfe63098f155b97e2a9514e1a862baa9c22bc1
Alexander <alxndr+github@gmail.com>
2021-02-17 23:02:57 -0800

    ch 6: initial implementation of `numbered?` and `value`

--- little-schemer/ch-6-shadows.scm
+++ little-schemer/ch-6-shadows.scm
@@ -1,10 +1,38 @@
+(load "ch-1-toys.scm") ; define atom?
+(load "ch-4-numbers-games.scm") ; define exponent
+
 (define numbered?
   (lambda (aexp)
     (cond
-      ((atom? (car aexp))
-       ; if car aexp is a number, then: next val must be null-or-operator,  and val after must be number-or-list
-       ; if car aexp is operator.......... #f ?? cause invalid syntax............
-       )
+      ((atom? aexp)
+       (number? aexp))
+
+      ((eq? (car (cdr aexp)) (quote +))
+       (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp))))))
+      ((eq? (car (cdr aexp)) (quote *))
+       (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp))))))
+      ((eq? (car (cdr aexp)) (quote ^))
+       (and (numbered? (car aexp)) (numbered? (car (cdr (cdr aexp))))))
+
       (else
-        ()
-        ))))
+        (#f)))))
+
+(define value
+  (lambda (aexp)
+    (cond
+      ((numbered? aexp)
+       (cond
+         ((atom? aexp) aexp)
+
+         ((eq? (car (cdr aexp)) (quote +))
+          (plus (car aexp) (car (cdr (cdr aexp)))))
+
+         ((eq? (car (cdr aexp)) (quote *))
+          (times (car aexp) (car (cdr (cdr aexp)))))
+
+         ((eq? (car (cdr aexp)) (quote ^))
+          (exponent (car aexp) (car (cdr (cdr aexp)))))
+
+         (else #f))
+       )
+      (else #f))))
