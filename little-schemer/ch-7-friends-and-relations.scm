3f6723358ccaf0f5c3156f9dc8ed121fcc3c88c1
Alexander <alxndr+github@gmail.com>
2021-02-18 23:28:26 -0800

    ch 7: naive `union`

--- little-schemer/ch-7-friends-and-relations.scm
+++ little-schemer/ch-7-friends-and-relations.scm
@@ -55,3 +55,13 @@
       (else
         (intersect (cdr set1) set2))
       )))
+
+(define union
+  (lambda (set1 set2)
+    (cond
+      ((null? set1)
+       set2)
+      ((member? (car set1) set2)
+       (union (cdr set1) set2))
+      (else
+        (cons (car set1) (union (cdr set1) set2))))))
