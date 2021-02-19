34b004969ad1d1e336c5873f54f0cc3db9d9e264
Alexander <alxndr+github@gmail.com>
2021-02-18 22:47:04 -0800

    ch 7: refactor `subset` using `or`/`and` rather than `cond`

--- little-schemer/ch-7-friends-and-relations.scm
+++ little-schemer/ch-7-friends-and-relations.scm
@@ -25,10 +25,8 @@
 
 (define subset?
   (lambda (set1 set2)
-    (cond
-      ((null? set1)
-       #t)
-      ((member? (car set1) set2)
-       (subset? (cdr set1) set2))
-      (else
-        #f))))
+    (or
+      (null? set1)
+      (and
+        (member? (car set1) set2)
+        (subset? (cdr set1) set2)))))
