ef074549074e794a61cab865e8f684e775bd800f
Alexander <alxndr+github@gmail.com>
2021-02-23 23:55:19 -0800

    ch 8: `rember-f` using `quasiquote` & `unquote`

---
+++ little-schemer/ch-8-lambda-the-ultimate.scm
@@ -0,0 +1,11 @@
+(define rember-f
+  (lambda (f a l)
+    (printf "~s ?? ~s ~s ~n" f a (car l))
+    (cond
+      ((null? l)
+       '())
+      ((apply f (quasiquote (a (unquote (car l)))))
+       (cdr l))
+      (else
+        (cons (car l)
+              (rember-f f a (cdr l)))))))
