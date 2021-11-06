62905070e8b122b567a54eb2a7b4b5601aab9f13
Alexander <alxndr+github@gmail.com>
2021-11-06 13:48:11 -0700

    TLS ch8: curry functions

--- little-schemer/ch-8-lambda-the-ultimate.scm
+++ little-schemer/ch-8-lambda-the-ultimate.scm
@@ -8,3 +8,11 @@
       (else
         (cons (car lst)
               (rember-f test-fun atm (cdr lst)))))))
+
+(define eq?-curry
+  (lambda (a)
+    (lambda (x)
+      (eq? x a))))
+
+(define eq?-salad
+  (eq?-curry 'salad))
