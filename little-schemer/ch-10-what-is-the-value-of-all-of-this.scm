6cf34fac64f4370da3ccac33520674fed7abd1b3
Alexander <alxndr+github@gmail.com>
2021-11-07 21:56:16 -0800

    TLS ch10: `lookup-in-table`

--- little-schemer/ch-10-what-is-the-value-of-all-of-this.scm
+++ little-schemer/ch-10-what-is-the-value-of-all-of-this.scm
@@ -1,5 +1,6 @@
 ; "An entry is a pair of lists whose first list is a set.
 ; "…the two lists must be of equal length."
+
 (define lookup-in-entry
   (lambda (name entry fn-not-found)
     ; find atom in 2nd list which matches the position of name in 1st list
@@ -20,3 +21,19 @@
                                 (cdr names)
                                 (cdr vals)
                                 fn-not-found)))))
+
+; "A table [aka environment] is a list of entries."
+
+;(define extend-table
+;  ; "…takes an entry and a table and … put[s] the new entry in front of the old table"
+;  (lambda (entry table)
+;    null))
+
+(define lookup-in-table
+  (lambda (name table fn-not-found)
+    (cond
+      ((null? table) (fn-not-found name))
+      (else
+        (lookup-in-entry name
+                         (car table)
+                         (lambda (name) (lookup-in-table name (cdr table) fn-not-found)))))))
