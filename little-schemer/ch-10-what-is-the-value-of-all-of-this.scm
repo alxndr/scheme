09cd04a3fca9026e9b55355dc7a6e1fad3e9b38f
Alexander <alxndr+github@gmail.com>
2021-11-08 19:04:02 -0800

    TLS ch10: starting to build an execution environment...

--- little-schemer/ch-10-what-is-the-value-of-all-of-this.scm
+++ little-schemer/ch-10-what-is-the-value-of-all-of-this.scm
@@ -24,10 +24,9 @@
 
 ; "A table [aka environment] is a list of entries."
 
-;(define extend-table
-;  ; "…takes an entry and a table and … put[s] the new entry in front of the old table"
-;  (lambda (entry table)
-;    null))
+(define extend-table
+ ; "…takes an entry and a table and … put[s] the new entry in front of the old table"
+ cons)
 
 (define lookup-in-table
   (lambda (name table fn-not-found)
@@ -36,4 +35,81 @@
       (else
         (lookup-in-entry name
                          (car table)
-                         (lambda (name) (lookup-in-table name (cdr table) fn-not-found)))))))
+                         (lambda (name)
+                           (lookup-in-table name
+                                            (cdr table)
+                                            fn-not-found)))))))
+
+(define build
+  (lambda (t v)
+    (cons t (cons v '()))))
+
+(define *const
+  (lambda (e table)
+    (cond
+      ((number? e) e)
+      ((eq? e #t) #t)
+      ((eq? e #f) #f)
+      (else (build 'primitive e)))))
+
+(define atom-to-action
+  (lambda (atm)
+    (cond
+      ((number? atm) *const)
+      ((eq? e #t) *const)
+      ((eq? e #f) *const)
+      ((eq? e 'cons) *const)
+      ((eq? e 'car) *const)
+      ((eq? e 'cdr) *const)
+      ((eq? e 'null?) *const)
+      ((eq? e 'eq?) *const)
+      ((eq? e 'atom?) *const)
+      ((eq? e 'zero?) *const)
+      ((eq? e 'add1) *const)
+      ((eq? e 'sub1) *const)
+      ((eq? e 'number?) *const)
+      (else *identifier))))
+
+(define text-of
+  second)
+
+(define *quote
+  (lambda (e table)
+    (text-of e)))
+
+(define *identifier
+  (lambda (e table)
+    (lookup-in-table e
+                     table
+                     (lambda (name) '(NOTFOUND (unquote name))))))
+
+(define *lambda
+  (lambda (e table)
+    (build 'non-primitive
+           (cons table (cdr e)))))
+
+(define list-to-action
+  (lambda (e)
+    (cond
+      ((atom? (car e))
+       (cond
+         ((eq? (car e) 'quote) *quote)
+         ((eq? (car e) 'lambda) *lambda)
+         ((eq? (car e) 'cond) *cond)
+         (else *application)))
+      (else *application))))
+
+(define expression-to-action
+  (lambda (e)
+    (cond
+      ((atom? e) (atom-to-action e))
+      (else (list-to-action e)))))
+
+(define meaning
+  (lambda (e table)
+    ((expression-to-action e) e table)))
+
+(define value
+  ; "…approximates the function `eval` in Scheme"
+  (lambda (e)
+    (meaning e '())))
